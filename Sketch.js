afd293f577046bd93fab343a7f16ff1fc08132a1
Lyra7770 <lyramir955@gmail.com>
2025-06-10 12:48:39 -0400

    Sketch.js

--- Sketch.js
+++ Sketch.js
@@ -1,48 +1,69 @@
 let t = 0;
+let burbujas = [];
 let paleta = [
   [191, 165, 149], // #BFA595
   [89, 32, 27], // #59201B
   [191, 132, 126], // #BF847E
-  [140, 55, 55], // #8C3737
-  [13, 13, 13] // #0D0D0D
+  [140, 55, 55] // #8C3737
 ];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
+  for (let i = 0; i < 300; i++) {
+    burbujas.push(new Burbuja());
+  }
 }
 
 function draw() {
-  background(paleta[4][0], paleta[4][1], paleta[4][2], 50); // fondo con transparencia
-
+  background(13, 13, 13); // #0D0D0D
   t += 0.01;
 
-  for (let i = 0; i < 250; i++) {
-    let x = noise(i * 0.1, t) * width;
-    let y = noise(i * 0.2, t + 100) * height;
-    let r = noise(i * 0.05, t + 200) * 50 + 15;
+  for (let b of burbujas) {
+    b.actualizar();
+    b.dibujar();
+  }
+}
 
-    let col = random(paleta);
-    fill(col[0], col[1], col[2], 30);
-    ellipse(x, y, r, r);
+class Burbuja {
+  constructor() {
+    this.r = random(20, 60);
+    this.xoff = random(1000);
+    this.yoff = random(1000);
+    this.col = random(paleta);
+    this.x = noise(this.xoff, t) * width;
+    this.y = noise(this.yoff, t + 100) * height;
   }
 
-  // Interacción con mouse o touch
-  if (mouseIsPressed || touches.length > 0) {
-    let cant = 10;
-    let x = mouseIsPressed ? mouseX : touches[0].x;
-    let y = mouseIsPressed ? mouseY : touches[0].y;
+  actualizar() {
+    this.x = noise(this.xoff, t) * width;
+    this.y = noise(this.yoff, t + 100) * height;
+  }
 
-    for (let j = 0; j < cant; j++) {
-      let angle = random(TWO_PI);
-      let rad = random(10, 60);
-      let rx = x + cos(angle) * rad;
-      let ry = y + sin(angle) * rad;
-      let r = random(8, 20);
-      let col = random(paleta.slice(0, 4)); // evita fondo oscuro
-      fill(col[0], col[1], col[2], 80);
-      ellipse(rx, ry, r, r);
-    }
+  dibujar() {
+    let d = dist(mouseX, mouseY, this.x, this.y);
+    let alpha = map(d, 0, 300, 180, 10);
+    fill(this.col[0], this.col[1], this.col[2], alpha);
+    ellipse(this.x, this.y, this.r, this.r);
+  }
+}
+
+function mousePressed() {
+  agregarBurbuja(mouseX, mouseY);
+}
+
+function touchStarted() {
+  agregarBurbuja(mouseX, mouseY);
+}
+
+function agregarBurbuja(x, y) {
+  for (let i = 0; i < 5; i++) {
+    let b = new Burbuja();
+    b.xoff = random(1000);
+    b.yoff = random(1000);
+    b.x = x + random(-30, 30);
+    b.y = y + random(-30, 30);
+    burbujas.push(b);
   }
 }
 
